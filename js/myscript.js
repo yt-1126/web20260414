b23fc6944b1952037efc8b03635314250ad65d30
yt-1126 <amy8080117@gmail.com>
2026-04-28 11:33:16 +0800

    修改練習js 0428

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
-var num = [1, 5, 2, 8, 3];
+var num = [1, 5, 2, 8, 3,53,210];
 
 var max = -99999;
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < num.length; i++) {
     if (max < num[i]) {
         max = num[i];
     }
